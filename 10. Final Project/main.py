d735b6c5998c21264c2f30af9b2b4a0684208cb1
aldomrodriguez <rodriguez.aldo.91@hotmail.com>
2025-02-28 20:13:02 -0300

    carga de proyecto final

--- 10. Final Project/main.py
+++ 10. Final Project/main.py
@@ -4,13 +4,17 @@ import json
 import random
 import os
 from dotenv import load_dotenv
+from keep_alive import keep_alive
+
+keep_alive()
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+
 def get_meme():
     response = requests.get('https://api.imgflip.com/get_memes')
-    
+
     if response.status_code != 200:
         return "No se pudo obtener un meme en este momento. 😢"
 
@@ -22,7 +26,9 @@ def get_meme():
 
     return meme["url"]  # Retorna solo la URL de la imagen
 
+
 class MyClient(discord.Client):
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
@@ -33,7 +39,8 @@ class MyClient(discord.Client):
 
         if message.content.startswith('$meme'):
             meme_url = get_meme()
-            await message.channel.send(meme_url)  
+            await message.channel.send(meme_url)
+
 
 # Configuración de intents
 intents = discord.Intents.default()
@@ -41,5 +48,4 @@ intents.message_content = True
 
 # Crear instancia del bot con intents
 client = MyClient(intents=intents)
-client.run(TOKEN) # Replace with your own token.
-
+client.run(TOKEN)  # Replace with your own token.
